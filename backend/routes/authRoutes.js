dab454a9f42daa637ac1da339242833786fc8d1b
Johann Abraham Argueta Mendoza <87659137+Chorizo1414@users.noreply.github.com>
2025-09-23 21:42:21 -0600

    cambios en estructura de còdigo backup

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-// --- CORRECCIÓN CLAVE AQUÍ ---
-// Usamos desestructuración { loginUser } para importar la función específica.
 const { loginUser } = require('../controllers/authController');
-
-// La ruta ahora recibe una función válida y el servidor no se detendrá.
 router.post('/login', loginUser);
-
 module.exports = router;
